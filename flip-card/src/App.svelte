6d208181ae15f10f6c7e7b715089d15c0f60b28a
OguzOzcan <e.oguz.ozcan@gmail.com>
2022-01-18 15:37:36 +0300

    first commit

---
+++ flip-card/src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+  import FlipCard from "./components/FlipCard.svelte";
+  import { Col, Container, Row } from "sveltestrap";
+  import Fa from "svelte-fa/src/fa.svelte";
+  import { faFlag, faPencilAlt, faCog, faInfo } from "@fortawesome/free-solid-svg-icons";
+  import * as dataText from "./data/data.json";
+  let textBundle = dataText.default
+  console.log("textBundle",JSON.stringify(textBundle));
+</script>
+
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+  />
+</svelte:head>
+
+<Container>
+  <Row>
+    {#each textBundle as sideCard}
+      <Col>
+        <FlipCard
+          sideCardText={sideCard.sideCardText}
+          backCardText={sideCard.backCardText}
+          sideCardIcon={faFlag}
+        />
+      </Col>
+    {/each}
+  </Row>
+</Container>
---
+++ flip-card/src/components/FlipCard.svelte
@@ -0,0 +1,124 @@
+<script>
+	import Fa from 'svelte-fa/src/fa.svelte'
+	import { faFlag } from '@fortawesome/free-solid-svg-icons'
+	export let sideCardText;
+	export let backCardText;
+	export let sideCardIcon;
+
+	let flipped = false
+	
+	function flip(node, {
+		delay = 0,
+		duration = 650
+	}) {
+		return {
+			delay,
+			duration,
+			css: (t, u) => `
+				transform: rotateY(${1 - (u * 180)}deg);
+				opacity: ${1 - u};
+			`
+		};
+	}
+
+</script>
+
+
+<style>
+	
+	* {
+
+		text-align: center;
+
+		}
+
+
+	.card-container {
+		position: relative;
+		height: 300px;
+		width: 300px;
+		display: inline-block;
+
+	}
+	
+	.card {
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		perspective: 600;
+		border:none;
+	}
+	
+	.side {
+		position: absolute;
+		height: 100%;
+		width: 100%;
+		background-color: rgb(255, 0, 0);
+		overflow: hidden;
+		color: #fff;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+		font-size: large;
+  		font-weight: bold;
+		  
+		  
+  		
+	}
+
+	.side-text {
+		margin-left: 70px;
+		margin-right: 70px;
+		margin-top: 0px;
+		width: 150px;
+		
+	}
+	
+	.back {
+		display: flex;
+		position: absolute;
+		position: relative;
+		height: 100%;
+		width: 100%;
+		background-color: rgb(255, 0, 0);
+		overflow: hidden;
+		color: #fff;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+		font-size: large;
+  		font-weight: bold;
+		background-image: url('https://xtratheme.com/elementor/interior/wp-content/uploads/sites/3/2017/08/img5-1.jpg');
+		background-position: center center;
+  		background-size: contain;
+		
+  		
+	}
+
+	.back p {
+  		color: rgb(255, 255, 255) !important;
+  		font-weight: bold;
+ 		opacity: 1 !important;
+		
+	}
+	.icon {
+		margin-top: 75px;
+		
+	}
+</style>
+
+<div class="card-container" on:mouseenter={() => flipped = !flipped} on:mouseleave={() => flipped = !flipped}>
+	<div class="card">
+		{#if flipped}
+		<div class="side" transition:flip>
+			<h1 class="icon"><Fa icon={sideCardIcon} size="xs"/></h1>
+
+				<p class="side-text">{sideCardText}</p>
+		</div>
+		{:else}
+		<div class="back" transition:flip>
+			<p>{backCardText} </p>
+		</div>
+		{/if}
+	</div>
+</div>
